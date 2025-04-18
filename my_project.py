c956adfadad3f99dceee115ebea87801a66aaa80
Airushmeos <benellirider@hotmail.com>
2025-04-18 10:10:47 +0200

    Create my_project.py

---
+++ my_project.py
@@ -0,0 +1,12 @@
+# hello_world.py
+
+# Eine einfache Ausgabe
+print("Hello, World!")
+
+# Berechnung: Addition von zwei Zahlen
+zahl_1 = 5
+zahl_2 = 7
+ergebnis = zahl_1 + zahl_2
+
+# Ausgabe des Ergebnisses
+print(f"Das Ergebnis der Addition von {zahl_1} und {zahl_2} ist {ergebnis}.")
